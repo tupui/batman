70e8cb8254597bfd2c28fd46fe933cffe1bb8da4
Romain Dupuis <dupuis@cerfacs.fr>
2016-02-18 09:39:50 +0100

    Modification of empile.sh for 1D and 2D test-case in order to sort sampling by chronological order

--- test-cases/Function_1D/post/empile.sh
+++ test-cases/Function_1D/post/empile.sh
@@ -1,3 +1,4 @@
 cat -t ../output/static_sobol_task_0/predictions/*/header.py > HEAD.dat 
 cat -t ../output/static_sobol_task_0/predictions/*/function.dat > FUNC.dat
-cat -t ../output/static_sobol_task_0/snapshots/*/jpod-data/header.py > SAMP.dat
+#cat -t ../output/static_sobol_task_0/snapshots/*/jpod-data/header.py > SAMP.dat
+cat $(find ../output/static_sobol_task_0/snapshots/*/jpod-data/header.py -name "*" | sort -V) > SAMP.dat
