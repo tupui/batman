6b8e925c132b9752fa828a4bb7a72f010e4079bc
Pamphile Roy <roy@cerfacs.fr>
2016-06-17 14:47:50 +0200

    Fix multimodes Kriging model

--- kernel/jpod/algebra/dacekriging.py
+++ kernel/jpod/algebra/dacekriging.py
@@ -1,13 +1,18 @@
 try:
-    import numpy as np 
     from sklearn.gaussian_process import GaussianProcessRegressor
     from sklearn.gaussian_process.kernels import RBF
+except ImportError:
+    raise NotImplementedError('No Kriging available, without scikits.learn module.')
+import numpy as np 
+import logging
 
-    class Kriging():
-        """Kriging interpolation using Gaussian Process method."""
+class Kriging():
+    """Kriging interpolation using Gaussian Process method."""
 
-        def __init__(self, input, output):
-	    """Create the predictor.
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, input, output):
+        """Create the predictor.
 
             Uses input and output to find a predictor using Gaussian Process.
 
@@ -15,16 +20,19 @@ try:
 
             :param array input: The input used to generate the output.
             :param array output: The observed data.
-	    
-	    """
-	    kernel = 1.0 * RBF(length_scale=10., length_scale_bounds=(0.01, 100.))
-	    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-            self.data = []
-            for column in output.T:
-                self.data += [ gp.fit(input, column) ]
-    
-        def evaluate(self, point):
-	    """Make a prediction. 
+        
+        """
+        self.kernel = 1.0 * RBF(length_scale=10., length_scale_bounds=(0.01, 100.))
+        self.data = []
+        self.hyperparameter = []
+        for column in output.T:
+            gp = GaussianProcessRegressor(kernel=self.kernel, n_restarts_optimizer=10)
+            self.data += [ gp.fit(input, column) ]
+            self.hyperparameter += [np.exp(gp.kernel_.theta)]
+        self.logger.debug("Hyperparameters: {}".format(self.hyperparameter))
+
+    def evaluate(self, point):
+        """Make a prediction. 
 
             From a point, make a new prediction.
 
@@ -32,16 +40,10 @@ try:
             :return: The predictions.
             :rtype: list
 
-            """
-            point_array = np.asarray(point).reshape(1, len(point))
-            v = np.ndarray((len(self.data)))
-            for i, gp in enumerate(self.data):
-                v[i] = gp.predict(point_array, return_std=False, return_cov=False)
-            return v
+        """
+        point_array = np.asarray(point).reshape(1, len(point))
+        v = np.ndarray((len(self.data)))
+        for i, gp in enumerate(self.data):
+            v[i] = gp.predict(point_array, return_std=False, return_cov=False)
+        return v
 
-except ImportError:
-    class Kriging():
-
-        def __init__(self, parameters, VS):
-            raise NotImplementedError(
-                'No Kriging available, without scikits.learn module.')
