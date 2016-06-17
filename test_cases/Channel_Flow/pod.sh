b9ce7dd5e6134bbb26b7d745f5f2a766cec14c95
Pamphile Roy <roy@cerfacs.fr>
2016-06-17 16:01:41 +0200

    Remove --set and remove my mail in launch scripts

--- test_cases/Channel_Flow/pod.sh
+++ test_cases/Channel_Flow/pod.sh
@@ -3,11 +3,11 @@
 #SBATCH --time=00:10:00
 #SBATCH --nodes=1 --ntasks-per-node=1
 #SBATCH --job-name=JPOD
-#SBATCH --mail-user roy@cerfacs.fr
-#SBATCH --mail-type all
+##SBATCH --mail-user roy@cerfacs.fr
+##SBATCH --mail-type all
 #SBATCH --share
 
-module load application/openturns/1.6rc1
+module load application/openturns/1.7
 module load python/2.7
 
 cd ${SLURM_SUBMIT_DIR}
--- test_cases/Channel_Flow/stat.sh
+++ test_cases/Channel_Flow/stat.sh
@@ -3,14 +3,14 @@
 #SBATCH --time=00:12:00
 #SBATCH --nodes=1 --ntasks-per-node=1
 #SBATCH --job-name=JPOD
-#SBATCH --mail-user roy@cerfacs.fr
-#SBATCH --mail-type all
+##SBATCH --mail-user ...@cerfacs.fr
+##SBATCH --mail-type all
 #SBATCH --share
 
-module load application/openturns/1.6rc1
+module load application/openturns/1.7
 module load python/2.7
 
 cd ${SLURM_SUBMIT_DIR}
 
-python ~/JPOD/kernel/jpod/ui.py scripts/task.py -o output/task -u > jpod_stat.log
+python ~/JPOD/kernel/jpod/ui.py scripts/task.py -o output/task -n -u > jpod_stat.log
 
