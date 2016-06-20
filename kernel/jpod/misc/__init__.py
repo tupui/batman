6da9b68e5c1660afd1ab5c97dfeef1b02cc66885
Pamphile Roy <roy@cerfacs.fr>
2016-06-20 10:21:49 +0200

    Remove mpi4py and clean misc

--- kernel/jpod/misc/__init__.py
+++ kernel/jpod/misc/__init__.py
@@ -5,9 +5,4 @@ def clean_path(path):
     """Return an absolute and normalized path."""
     return os.path.abspath(os.path.normpath(path))
 
-try:
-    # available from python 2.7
-    from collections import OrderedDict
-except ImportError:
-    # back port for python 2.5-2.6
-    from ordereddict import OrderedDict
+from collections import OrderedDict
