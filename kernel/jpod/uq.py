Fix Q2 computation in uq.py

kernel/jpod/uq.py
@@ -241,19 +241,20 @@ class UQ:
         s_err_l2_total = np.sqrt(np.sum((s_total_th - indices[2]) ** 2))
 
         eval_mean = np.zeros(self.output_len)
+        eval_pod = np.empty(self.points_sample)
         err_max = 0.
         err_l2 = 0.
-        for _, j in enumerate(self.sample):
-            eval_ref = np.array([model_ref(j)[i] for i in range(self.output_len)])
-            eval_pod = np.array([self.model(j)[i] for i in range(self.output_len)])
+       
+        for i, j in enumerate(self.sample):
+            eval_ref = np.array(model_ref(j))
+            eval_pod[i] = np.array(self.model(j))
             eval_mean = eval_mean + eval_ref
-            err_max = max(err_max, max(abs(eval_pod - eval_ref)))
-            err_l2 = err_l2 + np.sum((eval_pod - eval_ref) ** 2)
+            err_max = max(err_max, max(abs(eval_pod[i] - eval_ref)))
+            err_l2 = err_l2 + np.sum((eval_pod[i] - eval_ref) ** 2)
         eval_mean = eval_mean / self.points_sample
         eval_var = 0.
-        for _, j in enumerate(self.sample):
-            eval_ref = np.array([self.model(j)[i] for i in range(self.output_len)])
-            eval_var = eval_var + np.sum((eval_mean - eval_ref) ** 2)
+        for i, j in enumerate(self.sample):
+            eval_var = eval_var + np.sum((eval_mean - eval_pod[i]) ** 2)
         err_q2 = 1 - err_l2 / eval_var
 
         self.logger.info("\n----- POD error -----")