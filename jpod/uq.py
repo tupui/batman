5443c63d223d7a6492b43fa821fd3dca8c6d3242
Pamphile Roy <roy@cerfacs.fr>
2016-08-31 11:35:04 +0200

    Revert Q2 with eval_ref

--- jpod/uq.py
+++ jpod/uq.py
@@ -242,20 +242,20 @@ class UQ:
         s_err_l2_total = np.sqrt(np.sum((s_total_th - indices[2]) ** 2))
 
         eval_mean = np.zeros(self.output_len)
-        eval_pod = []
+        eval_ref = []
         err_max = 0.
         err_l2 = 0.
 
         for i, j in enumerate(self.sample):
-            eval_ref = np.array(model_ref(j))
-            eval_pod.append(np.array(self.model(j)))
-            eval_mean = eval_mean + eval_ref
-            err_max = max(err_max, max(abs(eval_pod[i] - eval_ref)))
-            err_l2 = err_l2 + np.sum((eval_pod[i] - eval_ref) ** 2)
+            eval_ref.append(np.array(model_ref(j)))
+            eval_pod = np.array(self.model(j))
+            eval_mean = eval_mean + eval_ref[i]
+            err_max = max(err_max, max(abs(eval_pod - eval_ref[i])))
+            err_l2 = err_l2 + np.sum((eval_pod - eval_ref[i]) ** 2)
         eval_mean = eval_mean / self.points_sample
         eval_var = 0.
         for i, _ in enumerate(self.sample):
-            eval_var = eval_var + np.sum((eval_mean - eval_pod[i]) ** 2)
+            eval_var = eval_var + np.sum((eval_mean - eval_ref[i]) ** 2)
         err_q2 = 1 - err_l2 / eval_var
 
         self.logger.info("\n----- POD error -----")
