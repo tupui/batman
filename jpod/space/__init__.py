cb10f087bf51b32571e3a6e435a9660ad9ffcc4f
Pamphile Roy <roy@cerfacs.fr>
2016-12-05 17:39:57 +0100

    Clean Point

--- jpod/space/__init__.py
+++ jpod/space/__init__.py
@@ -4,7 +4,7 @@ Space module
 """
 
 from .space import SpaceBase, Space, FullSpaceError, AlienPointError, UnicityError
-from .point import Point, DimensionError
+from .point import Point
 from .refiner import Refiner
 
-__all__ = ["SpaceBase", "Space", "Point", "DimensionError", "Refiner", "FullSpaceError", "AlienPointError", "UnicityError"]
+__all__ = ["SpaceBase", "Space", "Point", "Refiner", "FullSpaceError", "AlienPointError", "UnicityError"]
--- jpod/space/point.py
+++ jpod/space/point.py
@@ -1,25 +1,23 @@
-import os
-import re
+# coding: utf8
+"""
+A simple class of points in space.
+
+A point is a tuple of coordinates, thus this class derives from the tuple class.
+A point can be created either from integer or float coordinates, they are stored internally as float values.
+Two points can be compared with the usual `==` or `!=`, it can be approximative in the sense that two close points can be equals. The comparison is done by computing the distance of the points and checking it against a predefined threshold.
+"""
 import logging
-import numpy as N
-
-
-class DimensionError(Exception):
-    """Exception for bad data dimension."""
-    pass
+import numpy as np
 
 
 class Point(tuple):
-    """A simple class of points in space.
 
-    A point is a tuple of coordinates, thus this class derives from the tuple class.
-    A point can be created either from integer or float coordinates, they are stored internally as float values.
-    Two points can be compared with the usual `==` or `!=`, it can be approximative in the sense that two close points can be equals. The comparison is done by computing the distance of the points and checking it against a predefined threshold.
-    """
+    """Point class."""
+
+    logger = logging.getLogger(__name__)
 
     threshold = 0.
     '''Maximum distance error when comparing 2 points.'''
-
 
     @classmethod
     def set_threshold(cls, threshold):
@@ -27,17 +25,18 @@ class Point(tuple):
         try:
             threshold = float(threshold)
             if threshold < 0:
-                raise TypeError
-        except TypeError:
-            raise ValueError('threshold must be a positive number.')
+                raise ValueError
+        except ValueError:
+            self.logger.exception("Threshold must be a positive real number.")
+            raise SystemExit
         else:
             cls.threshold = threshold
-
 
     def __new__(cls, coordinates):
         """Create a new object from `coordinates`.
 
-        The argument `coordinates` must be a collection that can be converted to a tuple.
+        The argument `coordinates` must be a collection that can be converted
+        to a tuple.
         """
         # check and eventually convert int to float
         coords = []
@@ -45,14 +44,15 @@ class Point(tuple):
             try:
                 coords += [float(c)]
             except ValueError:
-                msg = 'coordinate value must be a real number : %s'%c
-                raise ValueError(msg)
+                self.logger.exception("Coordinate values must be real numbers: {}"
+                                      .format(c))
+                raise SystemExit
         return super(Point, cls).__new__(cls, coords)
 
 
     def __eq__(self, other):
-        # measure with the euclidian distance
-        return N.linalg.norm(N.array(self)-N.array(other)) \
+        """Compare using the euclidian distance"""
+        return np.linalg.norm(np.array(self) - np.array(other)) \
                <= self.__class__.threshold
 
 
