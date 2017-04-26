9ca710520e0dd99feb953b6bbdc72562d77b36ad
Pamphile Roy <roy@cerfacs.fr>
2017-04-26 19:11:36 +0200

    Evofusion for y=f(x) and start test

--- batman/surrogate/multifidelity.py
+++ batman/surrogate/multifidelity.py
@@ -14,6 +14,8 @@ Forrester, Sobester et al.: Multi-Fidelity Optimization via Surrogate Modelling.
 """
 import numpy as np
 import logging
+from .kriging import Kriging
+from ..functions import multi_eval
 
 
 class Evofusion(object):
@@ -23,4 +25,45 @@ class Evofusion(object):
     logger = logging.getLogger(__name__)
 
     def __init__(self, input, output):
-        pass
+        """Create the predictor.
+
+        Data are arranged as decreasing fidelity. Hence, ``input[0]``
+        corresponds to the highest fidelity.
+
+        :param ndarray input: The input used to generate the output. (fidelity, nb snapshots, nb parameters)
+        :param ndarray output: The observed data. (fidelity, nb snapshots, [nb output dim])
+
+        """
+        self.model_c = Kriging(input[1], output[1])
+        try:
+            input[0][0][0]
+        except (TypeError, IndexError):
+            pass
+        else:
+            input_array = np.array([np.array(input[0][:]).reshape(len(input[0]), -1),
+                                    np.array(input[1][:]).reshape(len(input[1]), -1)])
+
+        idx_cross_doe = np.where(input_array[0].reshape(-1,1) == input_array[1].reshape(1,-1))[1]
+        output_err = output[0] - output[1][idx_cross_doe]
+
+        self.model_err = Kriging(input[0], output_err)
+
+    @multi_eval
+    def evaluate(self, point):
+        """Make a prediction.
+
+        From a point, make a new prediction.
+
+        :param tuple(float) point: The point to evaluate.
+        :return: The predictions.
+        :rtype: lst
+        :return: The standard deviations.
+        :rtype: lst
+
+        """
+        f_c, sigma_c = self.model_c.evaluate(point)
+        f_err, sigma_err = self.model_err.evaluate(point)
+        f = f_c + f_err
+        sigma = sigma_c + sigma_err
+
+        return f, sigma
--- batman/tests/test_models.py
+++ batman/tests/test_models.py
@@ -3,6 +3,7 @@ import pytest
 import os
 import numpy as np
 import numpy.testing as npt
+import matplotlib.pyplot as plt
 import openturns as ot
 from batman.surrogate import (PC, Kriging, Evofusion, SurrogateModel)
 from batman.tasks import Snapshot
@@ -146,23 +147,46 @@ def test_SurrogateModel_class(tmp, ishigami_data, settings_ishigami):
 
 
 def test_evofusion(mufi_data):
-    _, _, dist, model, point, target_point, space, target_space = mufi_data
+    f_e, f_c, dist, model, point, target_point, space, target_space = mufi_data
 
     surrogate = Evofusion(space, target_space)
+    surrogate_e = Kriging(space[0], target_space[0])
+    surrogate_c = Kriging(space[1], target_space[1])
 
-    # Test one point evaluation
-    pred, _ = np.array(surrogate.evaluate(point))
-    assert pred == pytest.approx(target_point, 0.1)
 
-    # Test space evaluation
-    pred, _ = np.array(surrogate.evaluate(space[0]))
-    test_output = npt.assert_almost_equal(target_space[0], pred, decimal=1)
-    assert True if test_output is None else False
+    x = np.linspace(0, 1, 200).reshape(-1, 1)
 
-    # Compute predictivity coefficient Q2
-    def wrap_surrogate(x):
-        evaluation, _ = surrogate.evaluate(x)
-        return [evaluation]
-    surrogate_ot = ot.PythonFunction(1, 1, wrap_surrogate)
-    q2 = sklearn_q2(dist, model, surrogate_ot)
-    assert q2 == pytest.approx(1, 0.1)
+    pred_evo = np.array(surrogate.evaluate(x))[:, 0]
+    pred_e, _ = np.array(surrogate_e.evaluate(x))
+    pred_c, _ = np.array(surrogate_c.evaluate(x))
+
+    plt.plot(space[0], target_space[0], 'o', label=r'$y_e$')
+    plt.plot(space[1], target_space[1], '^', label=r'$y_c$')
+    plt.plot(x, f_e(x), ls='-', label=r'$f_e$')
+    plt.plot(x, f_c(x), ls='--', label=r'$f_c$')
+    plt.plot(x, pred_evo, ls='-.', label=r'$evofusion$')
+    plt.plot(x, pred_e, '>', markevery=20, ls=':', label=r'kriging through $y_e$')
+    plt.plot(x, pred_c, '<', markevery=20, ls=':', label=r'kriging through $y_c$')
+    plt.xlabel('x', fontsize=28)
+    plt.ylabel('y', fontsize=28)
+    plt.tick_params(axis='x', labelsize=26)
+    plt.tick_params(axis='y', labelsize=26)
+    plt.legend(fontsize=26, loc='upper left')
+    plt.show()
+
+    # # Test one point evaluation
+    # pred, _ = np.array(surrogate.evaluate(point))
+    # assert pred == pytest.approx(target_point, 0.1)
+
+    # # Test space evaluation
+    # pred, _ = np.array(surrogate.evaluate(space[0]))
+    # test_output = npt.assert_almost_equal(target_space[0], pred, decimal=1)
+    # assert True if test_output is None else False
+
+    # # Compute predictivity coefficient Q2
+    # def wrap_surrogate(x):
+    #     evaluation, _ = surrogate.evaluate(x)
+    #     return [evaluation]
+    # surrogate_ot = ot.PythonFunction(1, 1, wrap_surrogate)
+    # q2 = sklearn_q2(dist, model, surrogate_ot)
+    # assert q2 == pytest.approx(1, 0.1)
