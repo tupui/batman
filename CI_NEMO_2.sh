fe90fcc136836bca5b74bb4f62a63a832967e524
Cyril Fournier <fournier@cerfacs.fr>
2017-10-13 14:31:29 +0200

    C.I. scripts for concurrent execution on remote host

--- CI_NEMO_2.sh
+++ CI_NEMO_2.sh
@@ -6,25 +6,25 @@
 #SBATCH --ntasks-per-node=24
 #SBATCH --share
 
+commit=$1
+CI_HOME="$HOME/.ci_batman_2/$commit"
+CI_SCRATCH="/scratch/cfd/roy/CI_BATMAN_2_$commit"
+
+
 # On spike: send the job to the HPC scheduler and wait for completion
-if [ ${HOSTNAME:0:4} != 'nemo' ] && [ ${HOSTNAME:0:4} != 'node' ] ; then
-    nojob=$(ssh roy@nemo 'sbatch --share CI_NEMO_2.sh' | awk '{print $NF}') 
-    echo 'Job number: ' $nojob
+if [ ${HOSTNAME:0:4} != 'nemo' ] && [ ${HOSTNAME:0:4} != 'node' ]; then
+    nojob=$(ssh roy@nemo "sbatch --share $CI_HOME/CI_NEMO_2.sh $commit" | awk '{print $NF}') 
+    echo "Job number: $nojob"
     
-    r=0
-    while [ $r -eq 0 ]
-    do
-      resu=$(ssh roy@nemo "squeue -j $nojob -h -o %T")
-      if [ ! -z "$resu" ] ; then
-         sleep 30
-      else
-         r=1
-      fi
+    resu=$(ssh roy@nemo "squeue -j $nojob -h -o %T 2>/dev/null")
+    while [ "$resu" ]; do
+        sleep 30
+        resu=$(ssh roy@nemo "squeue -j $nojob -h -o %T 2>/dev/null")
     done
     
     ssh roy@nemo "cat slurm-$nojob.out"
-    status=$(ssh roy@nemo "sacct --format=state -j $nojob | awk 'NR>3 {print $1}'")
-    if [ $status = 'FAILED' ] ; then
+    stat=$(ssh roy@nemo "sacct --format=state -j $nojob | awk 'NR>3 {print $1}'")
+    if [ $stat = 'FAILED' ]; then
         exit 1
     else
         exit 0
@@ -32,16 +32,17 @@ if [ ${HOSTNAME:0:4} != 'nemo' ] && [ ${HOSTNAME:0:4} != 'node' ] ; then
 fi
 
 # On HPC:
-source activate bat_ci_2
+conda create --name bat_ci_2_$commit --clone bat_ci_2
+source activate bat_ci_2_$commit
 export PYTHONPATH=
 python --version
 
-SCRATCH='/scratch/cfd/roy'
-mkdir $SCRATCH/CI_BATMAN_2
-tar -xf batman_ci_2.tar -C $SCRATCH/CI_BATMAN_2/.
-rm batman_ci_2.tar CI_NEMO_2.sh
-cd $SCRATCH/CI_BATMAN_2/batman
+mkdir $CI_SCRATCH
+tar -xf $CI_HOME/batman_ci_2.tar -C $CI_SCRATCH/.
+rm $CI_HOME/batman_ci_2.tar $CI_HOME/CI_NEMO_2.sh
+rmdir --ignore-fail-on-non-empty $CI_HOME
 
+cd $CI_SCRATCH/batman
 python setup.py build_fortran
 python setup.py install
 which batman
@@ -54,8 +55,9 @@ else
     fail=0
 fi
 
-pip uninstall -y batman
-rm -r $SCRATCH/CI_BATMAN_2
+source deactivate
+conda remove --name bat_ci_2_$commit --all
+rm -r $CI_SCRATCH
 
 if [ $fail -eq 1 ] ; then
     exit 1
