Fix scaling and dists None

batman/space/sampling.py
@@ -27,7 +27,6 @@ and the method.
 """
 import logging
 from scipy import stats
-from scipy.stats import randint
 from sklearn import preprocessing
 import numpy as np
 import openturns as ot
@@ -63,15 +62,15 @@ class Doe():
         self.n_sample = n_sample
         self.bounds = np.asarray(bounds)
         self.kind = kind
-        self.dim = bounds.shape[1]
+        self.dim = self.bounds.shape[1]
 
         self.scaler = preprocessing.MinMaxScaler()
         self.scaler.fit(self.bounds)
 
         if dists is None:
-            distribution = ot.ComposedDistribution(
-                [ot.Uniform(float(self.bounds[0][i]), float(self.bounds[1][i]))
-                 for i in range(self.dim)])
+            dists = [ot.Uniform(float(self.bounds[0][i]),
+                                float(self.bounds[1][i]))
+                     for i in range(self.dim)]
         else:
             try:
                 dists = [eval('ot.' + dist, {'__builtins__': None},
@@ -83,12 +82,9 @@ class Doe():
 
         if discrete is not None:
             # Creating uniform discrete distribution for OT
-            rv = randint(self.bounds[0, discrete], self.bounds[1, discrete] + 1)
-            points = ot.Sample(10000, 1)
-            for i in range(10000):
-                points[i] = (rv.rvs(),)
-
-            disc_dist = ot.UserDefined(points)
+            disc_list = [[i] for i in range(int(self.bounds[0, discrete]),
+                                            int(self.bounds[1, discrete] + 1))]
+            disc_dist = ot.UserDefined(disc_list)
 
             dists.pop(discrete)
             dists.insert(discrete, disc_dist)
@@ -142,7 +138,7 @@ class Doe():
             sample = ((np.floor_divide(sample, (1. / self.n_sample)) + 1)
                  - 0.5) / self.n_sample
         else:
-            sample = self.scaler.transform(sample)
+            sample = self.scaler.inverse_transform(sample)
 
         return sample
 

batman/tests/test_space.py
@@ -115,35 +115,45 @@ def test_space_evaluation(settings_ishigami):
 
 def test_doe():
     bounds = np.array([[0, 2], [10, 5]])
-    discrete_var = 0
     n = 5
 
-    doe = Doe(n, bounds, 'uniform', var=discrete_var)
+    doe = Doe(n, bounds, 'uniform', discrete=0)
     sample = doe.generate()
-    out = np.array([[0., 2.], [10., 2.], [0., 5.], [10., 5.]])
+    out = [[0., 2.], [10., 2.], [0., 5.], [10., 5.]]
     npt.assert_almost_equal(sample, out, decimal=1)
 
-    doe = Doe(n, bounds, 'discrete', var=discrete_var)
+    doe = Doe(n, bounds, 'halton', discrete=0)
     sample = doe.generate()
-    out = np.array([[5., 3.], [2., 4.], [8., 2.3], [1., 3.3], [6., 4.3]])
+    out = [[5., 3.], [2., 4.], [8., 2.3], [1., 3.3], [6., 4.3]]
+    npt.assert_almost_equal(sample, out, decimal=1)
+
+    doe = Doe(n, bounds, 'halton', discrete=1)
+    sample = doe.generate()
+    out = [[5, 3], [2.5 , 4], [7.5 , 2], [1.25, 3], [6.25, 5]]
+    npt.assert_almost_equal(sample, out, decimal=1)
+
+    doe = Doe(n, np.array([[0, 2, -2], [10, 5, -1]]), 'halton', discrete=1)
+    sample = doe.generate()
+    out = [[5, 3, -1.8 ], [2.5, 4, -1.6 ], [7.5, 2, -1.4 ],
+           [1.25, 3, -1.2 ], [6.25, 5, -1.96]]
     npt.assert_almost_equal(sample, out, decimal=1)
 
     doe = Doe(n, bounds, 'halton')
     sample = doe.generate()
-    out = np.array([[5., 3.], [2.5, 4.], [7.5, 2.3], [1.25, 3.3], [6.25, 4.3]])
+    out = [[5., 3.], [2.5, 4.], [7.5, 2.3], [1.25, 3.3], [6.25, 4.3]]
     npt.assert_almost_equal(sample, out, decimal=1)
 
-    doe = Doe(n, bounds, 'sobolscramble', var=discrete_var)
+    doe = Doe(n, bounds, 'sobolscramble', discrete=0)
     sample = doe.generate()
 
     doe = Doe(n, bounds, 'lhsopt')
     ot.RandomGenerator.SetSeed(123)
     sample = doe.generate()
-    out = np.array([[8.097, 3.646], [6.592, 4.806], [0.622, 2.909],
-                    [5.361, 2.162], [2.921, 4.041]])
+    out = [[8.097, 3.646], [6.592, 4.806], [0.622, 2.909],
+           [5.361, 2.162], [2.921, 4.041]]
     npt.assert_almost_equal(sample, out, decimal=1)
 
-    bounds = np.array([[15.0, 2500.0], [60.0, 6000.0]])
+    bounds = [[15.0, 2500.0], [60.0, 6000.0]]
 
     with pytest.raises(SystemError):
         dists = ['Um(15., 60.)', 'Normal(4035., 400.)']