Fix PDF scaling in DoE,replace fill by hist and fix CI deploy
Adapt some plotting functions to recommended style:

http://matplotlib.org/tutorials/introductory/usage.html#coding-styles

batman/tests/test_visualization.py
@@ -66,7 +66,7 @@ class TestHdr:
                              labels=labels,
                              x_common=np.linspace(1, 12, 12),
                              xlabel='Month of the year (-)',
-                             ylabel='Water surface temperature (C)')
+                             flabel='Water surface temperature (C)')
 
         assert len(figs) == 3
         assert len(axs) == 3
@@ -148,7 +148,7 @@ class TestHdr:
         hdr.f_hops(x_common=np.linspace(1, 12, 12),
                    labels=labels,
                    xlabel='Month of the year (-)',
-                   ylabel='Water surface temperature (C)',
+                   flabel='Water surface temperature (C)',
                    fname=os.path.join(tmp, 'f-HOPs.mp4'))
         hdr.f_hops(samples=10, fname=os.path.join(tmp, 'f-HOPs.mp4'))
         hdr.f_hops(samples=data, fname=os.path.join(tmp, 'f-HOPs.mp4'))
@@ -259,6 +259,7 @@ class TestPdf:
         reshow(fig_pdf)
         plt.plot([0, 10], [25, 25])
         plt.show()
+        plt.close()
 
     def test_pdf_nD_moments(self, tmp):
         pdf(data, xlabel='s', flabel='Y', moments=True,

batman/visualization/doe.py
@@ -10,6 +10,7 @@ from itertools import combinations_with_replacement
 import numpy as np
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
+import batman as bat
 from .uncertainty import kernel_smoothing
 
 
@@ -64,18 +65,18 @@ def doe(sample, plabels=None, resampling=0, multifidelity=False, fname=None,
             if i == j:  # diag
                 x_plot = np.linspace(min(sample[:, i]),
                                      max(sample[:, i]), 100)[:, np.newaxis]
-                scaler = preprocessing.MinMaxScaler().fit(sample[:, i, np.newaxis])
-                sample_scaled = scaler.transform(sample[:, i, np.newaxis])
-                _ks = kernel_smoothing(sample_scaled, False)
-                pdf = np.exp(_ks.score_samples(scaler.transform(x_plot)))
+                sample_ = sample[:, i, np.newaxis]
+                _ks = kernel_smoothing(sample_[:199], False)
+                pdf = np.exp(_ks.score_samples(x_plot))
                 ax.plot(x_plot, pdf)
-                ax.fill_between(x_plot[:, 0], pdf, [0] * x_plot.shape[0],
-                                color='gray', alpha=0.1)
+                ax.hist(sample_, 30, fc='gray', histtype='stepfilled',
+                        alpha=0.2, density=True)
+                ax.set_ylim(ymin=0)
             elif i < j:  # lower corners
-                ax.scatter(sample[0:len_sampling, i], sample[
-                    0:len_sampling, j], s=5, c='k', marker='o')
-                ax.scatter(sample[len_sampling:, i], sample[
-                    len_sampling:, j], s=5, c='r', marker='^')
+                ax.scatter(sample[0:len_sampling, i],
+                           sample[0:len_sampling, j], s=5, c='k', marker='o')
+                ax.scatter(sample[len_sampling:, i],
+                           sample[len_sampling:, j], s=5, c='r', marker='^')
 
             if i == 0:
                 ax.set_ylabel(plabels[j])
@@ -84,12 +85,6 @@ def doe(sample, plabels=None, resampling=0, multifidelity=False, fname=None,
 
             sub_ax.append(ax)
 
-    plt.tight_layout()
-
-    if fname is not None:
-        plt.savefig(fname, transparent=True, bbox_inches='tight')
-    elif show:
-        plt.show()
-    plt.close('all')
+    bat.visualization.save_show(fname, [fig])
 
     return fig, sub_ax

batman/visualization/kiviat.py
@@ -105,18 +105,20 @@ class Kiviat3D:
         self.x_ticks = np.cos(self.alphas)
         self.y_ticks = np.sin(self.alphas)
 
-    def plane(self, params, feval, idx, ax, fill=True):
+    def plane(self, ax, params, feval, idx, fill=True):
         """Create a Kiviat in 2D.
 
         From a set of parameters and the corresponding function evaluation,
         a 2D Kiviat plane is created. Create the mesh in polar coordinates and
         compute corresponding Z.
 
+        :param ax: Matplotlib AxesSubplot instances to draw to.
         :param array_like params: Parameters of the plane (n_params).
         :param feval: Function evaluation corresponding to :attr:`params`.
         :param idx: *Z* coordinate of the plane.
-        :param ax: Matplotlib AxesSubplot instances.
         :param bool fill: Whether to fill the surface.
+        :return: List of artists added.
+        :rtype: list.
         """
         params = self.scale.transform(np.asarray(params).reshape(1, -1))[0]
 
@@ -142,9 +144,11 @@ class Kiviat3D:
                                        alpha=0.3)
             polygon.set_color(color)
             polygon.set_edgecolor(color)
-            ax.add_collection3d(polygon)
+            out = ax.add_collection3d(polygon)
         else:
-            ax.plot(X, Y, Z, alpha=0.5, lw=3, c=color)
+            out = ax.plot(X, Y, Z, alpha=0.5, lw=3, c=color)
+
+        return out
 
     def _axis(self, ax):
         """n-dimentions axis definition.
@@ -152,26 +156,31 @@ class Kiviat3D:
         Create axis arrows along with annotations with parameters name and
         ticks.
 
-        :param ax: Matplotlib AxesSubplot instances.
+        :param ax: Matplotlib AxesSubplot instances to draw to.
+        :return: List of artists added.
+        :rtype: list.
         """
+        out = []
         for i in range(self.n_params):
             # Create axis
             a = Arrow3D([0, self.x_ticks[i]], [0, self.y_ticks[i]],
                         [self.z_offset, self.z_offset],
                         mutation_scale=20, lw=1, arrowstyle="-|>", color="k")
-            ax.add_artist(a)
+            out.append(ax.add_artist(a))
             # Annotate with plabels
-            ax.text(1.1 * self.x_ticks[i], 1.1 * self.y_ticks[i],
-                    self.z_offset, self.plabels[i],
-                    fontsize=14, ha='center', va='center', color='k')
+            out.append(ax.text(1.1 * self.x_ticks[i], 1.1 * self.y_ticks[i],
+                       self.z_offset, self.plabels[i],
+                       fontsize=14, ha='center', va='center', color='k'))
 
             # Add ticks with values
             for j, tick in enumerate(self.ticks[:, 0]):
                 x = tick * self.x_ticks[i]
                 y = tick * self.y_ticks[i]
-                ax.scatter(x, y, self.z_offset, c='k', marker='|')
-                ax.text(x, y, self.z_offset, self.ticks_values[i][j],
-                        fontsize=8, ha='right', va='center', color='k')
+                out.append(ax.scatter(x, y, self.z_offset, c='k', marker='|'))
+                out.append(ax.text(x, y, self.z_offset, self.ticks_values[i][j],
+                                   fontsize=8, ha='right', va='center', color='k'))
+
+        return ax
 
     def plot(self, fname=None, flabel='F', ticks_nbr=10, fill=True):
         """Plot 3D kiviat.
@@ -196,7 +205,7 @@ class Kiviat3D:
         cbar.set_label(flabel)
 
         for i, (point, f_eval) in enumerate(zip(self.sample, self.data)):
-            self.plane(point, f_eval[0], i, ax, fill)
+            self.plane(ax, point, f_eval[0], i, fill)
 
         self._axis(ax)
 
@@ -227,7 +236,7 @@ class Kiviat3D:
         ax.set_axis_off()
 
         for i, (point, f_eval) in enumerate(zip(self.sample, self.data)):
-            self.plane(point, f_eval[0], i, ax, fill)
+            self.plane(ax, point, f_eval[0], i, fill)
 
         self._axis(ax)
 
@@ -255,7 +264,7 @@ class Kiviat3D:
 
         with writer.saving(fig, fname, dpi=500):
             for i, (point, f_eval) in enumerate(zip(self.sample, self.data)):
-                self.plane(point, f_eval[0], i, ax, fill)
+                self.plane(ax, point, f_eval[0], i, fill)
                 # Rotate the view
                 ax.view_init(elev=-20 + elev_step * i, azim=i * azim_step)
 