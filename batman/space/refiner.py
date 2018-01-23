Fix sign in optimization results and for initial min value

batman/space/refiner.py
@@ -525,12 +525,14 @@ class Refiner(object):
         :return: The coordinate of the point to add.
         :rtype: lst(float).
         """
-        gen = [self.func(x) for x in self.scaler.inverse_transform(self.points)]
+        sign = 1 if extremum == 'min' else -1
+        gen = [self.func(x, sign=sign)
+               for x in self.scaler.inverse_transform(self.points)]
         arg_min = np.argmin(gen)
         min_value = gen[arg_min]
         min_x = self.points[arg_min]
-        self.logger.info('Current minimal value is: f(x)={} for x={}'
-                         .format(min_value, min_x))
+        self.logger.info('Current extrema value is: f(x)={} for x={}'
+                         .format(sign * min_value, min_x))
 
         # Do not check point is close on the discrete parameter
         if self.discrete is not None:
@@ -538,8 +540,6 @@ class Refiner(object):
             no_discrete[0].pop(self.discrete)
         else:
             no_discrete = None
-
-        sign = 1 if extremum == 'min' else -1
 
         @optimization(self.corners, self.discrete)
         def probability_improvement(x):

batman/space/space.py
@@ -292,21 +292,26 @@ class Space(list):
         self.max_points_nb = n + doe_cheap
         return doe_cheap
 
-    def optimization_results(self):
-        """Compute the optimal value."""
-        gen = [self.refiner.func(x) for x in self]
-        arg_min = np.argmin(gen)
-        min_value = gen[arg_min]
-        min_x = self[arg_min]
-        self.logger.info('New minimal value is: f(x)={} for x={}'
-                         .format(min_value, min_x))
+    def optimization_results(self, extremum):
+        """Compute the optimal value.
+
+        :param str extremum: minimization or maximization objective
+          ['min', 'max'].
+        """
+        sign = 1 if extremum == 'min' else -1
+        gen = [self.refiner.func(x, sign=sign) for x in self]
+        arg_extremum = np.argmin(gen)
+        _extremum = sign * gen[arg_extremum]
+        _x = self[arg_extremum]
+        self.logger.info('New extremum value is: f(x)={} for x={}'
+                         .format(_extremum, _x))
 
         bounds = np.array(self.corners).T
-        results = differential_evolution(self.refiner.func, bounds)
-        min_value = results.fun
-        min_x = results.x
+        results = differential_evolution(self.refiner.func, bounds, args=(sign,))
+        _extremum = sign * results.fun
+        _x = results.x
         self.logger.info('Optimization with surrogate: f(x)={} for x={}'
-                         .format(min_value, min_x))
+                         .format(_extremum, _x))
 
     def discrepancy(self, sample=None):
         """Compute the centered discrepancy.

batman/tests/test_space.py
@@ -234,7 +234,7 @@ def test_resampling(tmp, branin_data, settings_ishigami):
     npt.assert_almost_equal(optim_PI, [-2.2, 9.2], decimal=1)
 
     disc = refiner.discrepancy()
-    npt.assert_almost_equal(disc, [8.47, 9.1], decimal=1)
+    npt.assert_almost_equal(disc, [8.47, 12.4], decimal=1)
 
     extrema = np.array(refiner.extrema([])[0])
     npt.assert_almost_equal(extrema, [[-2.694, 2.331], [2.576, 2.242]], decimal=1)