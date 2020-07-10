e46f98f8c8dfc48c1aafa78bc8e5b9cdeb9795c5
Pamphile ROY <proy@bongfish.com>
2020-07-10 10:52:51 -1000

    Fix pytest and image

--- .github/continuous_integration/CI.sh
+++ .github/continuous_integration/CI.sh
@@ -1,23 +1,18 @@
 which python
 python --version
 
-python setup.py install --user
-which batman
+#python setup.py install
+#which batman
 
 # launch test suite and coverage
 
 circleci tests glob batman/tests/*.py | circleci tests split --split-by=timings | xargs coverage run -m pytest --basetemp=./TMP_CI
-
 #coverage run -m pytest --basetemp=./TMP_CI batman/tests # test_cases
-if [ $? -ne 0 ] ; then
-    fail=1
-else
-    fail=0
-fi
+
+status=$?
+
 coverage html
 mkdir test-reports
 mv htmlcov test-reports/coverage-report
 
-if [ $fail -eq 1 ] ; then
-    exit 1
-fi
+exit $status
